bf5bc530de796de961944539d8f7129d3cee0811
Wojciech Tylenda <wojciech.tylenda.96@gmail.com>
2024-12-12 21:28:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 
 const taskInputEl = document.getElementById("task-input");
 const taskListEl = document.getElementById("task-list");
+const deleteBtn = document.querySelectorAll(".delete-btn")
+
 
 function addTask() {
     const input = taskInputEl.value;
     taskListEl.innerHTML += `
-        <li>${input} <button id="delete-btn">DELETE TASK</button></li>
+        <li>${input} <button class="delete-btn">DELETE TASK</button></li>
     `
     taskInputEl.value = "";
 }
+
+taskListEl.addEventListener("click", function(event) {
+    if (event.target.classList.contains("delete-btn")) {
+        const listItem = event.target.parentElement;
+        listItem.remove();
+    }
+});
