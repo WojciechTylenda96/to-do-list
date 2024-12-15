7e91cfd38f724d4efd1e919f65b5f5c056649fa0
Wojciech Tylenda <wojciech.tylenda.96@gmail.com>
2024-12-15 19:41:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 
 const taskInputEl = document.getElementById("task-input");
 const taskListEl = document.getElementById("task-list");
-const deleteBtn = document.querySelectorAll(".delete-btn")
+const deleteBtn = document.querySelectorAll(".delete-btn");
+const checkBox = document.querySelectorAll("#checkbox");
 
+// add new task
 
 function addTask() {
     const input = taskInputEl.value;
     taskListEl.innerHTML += `
-        <li>${input} <button class="delete-btn">DELETE TASK</button></li>
+        <li><p>${input}</p> <button class="delete-btn">DELETE</button></li>
     `
     taskInputEl.value = "";
 }
+
+// delete task
 
 taskListEl.addEventListener("click", function(event) {
     if (event.target.classList.contains("delete-btn")) {
@@ -18,3 +22,6 @@ taskListEl.addEventListener("click", function(event) {
         listItem.remove();
     }
 });
+
+// mark task as done
+
